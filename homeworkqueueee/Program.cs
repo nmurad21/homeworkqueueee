720cb6a2dc145f6f0a7d9c4b073b659a7a12ad33
User <User@WIN-CUEGFOLCMBN>
2022-04-01 03:51:05 +0400

    Add project files.

---
+++ homeworkqueueee/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace homeworkqueueee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> queue = new Stack<string>();
+            queue.Push("Salam");
+            queue.Push("234");
+            queue.Push("Malas");
+            queue.Push("Hello");
+            queue.Push("qwert");
+            queue.Push("zxcv");
+            queue.Push("uiop");
+            queue.Push("luyrt");
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
